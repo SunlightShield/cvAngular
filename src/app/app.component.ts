a2351a3209605c40424844c8d358313df4e4f830
Sunlight <sespinozahenriquez@gmail.com>
2025-04-02 19:53:09 -0300

    instalacion de material angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { AboutComponent } from './about/about.component';
 import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AboutComponent],
+  imports: [RouterOutlet, AboutComponent, MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   standalone: true,
+  
 })
 export class AppComponent {
   title = 'cv-angular';
