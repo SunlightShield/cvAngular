06b17342ba8f7da999f7f9ae691c8ddff0348a8b
Sunlight <sespinozahenriquez@gmail.com>
2025-04-02 18:48:05 -0300

    primer componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { AboutComponent } from './about/about.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AboutComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  standalone: true,
 })
 export class AppComponent {
   title = 'cv-angular';
