9f956b7947f1855729352058500a26dd7ba69f7a
Sunlight <sespinozahenriquez@gmail.com>
2025-04-09 16:57:27 -0400

    avances

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,14 @@ import { Component } from '@angular/core';
 import { AboutComponent } from './about/about.component';
 import { RouterOutlet } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AboutComponent, MatButtonModule],
+  imports: [RouterOutlet, AboutComponent, MatButtonModule, MatCardModule, MatToolbarModule, MatIconModule, MatTooltipModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   standalone: true,
