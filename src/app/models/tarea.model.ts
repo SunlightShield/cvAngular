b2218fc02f61e861c49d9905bc853ec2843a0d29
Sunlight <sespinozahenriquez@gmail.com>
2025-04-11 13:41:09 -0400

    avances, tabla

--- src/app/models/tarea.model.ts
+++ src/app/models/tarea.model.ts
@@ -4,4 +4,5 @@ export interface Tarea {
     descripcion: string;
     completada: boolean;
     fechaCreacion: string;
+    semanaDelMes: number;
   }
--- src/app/pages/jira-copy/jira-copy.component.ts
+++ src/app/pages/jira-copy/jira-copy.component.ts
@@ -13,6 +13,9 @@ import {MatTableModule} from '@angular/material/table';
 })
 export class JiraCopyComponent implements OnInit {
 
+  diasDelMes: number[] = [];
+  semanas: number[] = [1, 2, 3, 4, 5];
+
   tareas: Tarea[] = [];
   displayedColumns: string[] = ['titulo', 'descripcion', 'completada'];
   constructor(private http: HttpClient) {}
@@ -23,10 +26,27 @@ export class JiraCopyComponent implements OnInit {
         next: (data) => {
           this.tareas = data;
           console.log('✅ Tareas desde API:', this.tareas);
+        
+          //para los dias 
+          const diasUnicos = Array.from(
+            new Set(this.tareas.map(t => new Date(t.fechaCreacion).getDate()))
+          );
+    
+          this.diasDelMes = diasUnicos.sort((a, b) => a - b);  
         },
         error: (err) => {
           console.error('❌ Error al obtener tareas:', err);
         }
       });
+  }
+
+
+  hayTarea(dia: number, semana: number): boolean {
+    return this.tareas.some(t => {
+      const fecha = new Date(t.fechaCreacion);
+      const diaTarea = fecha.getDate();
+      const semanaTarea = t.semanaDelMes;
+      return diaTarea === dia && semanaTarea === semana;
+    });
   }
 }
