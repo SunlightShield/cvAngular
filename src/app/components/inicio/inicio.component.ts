a624c70468b62a1074ace4bd7db3f5b70f83c5cb
Sunlight <sespinozahenriquez@gmail.com>
2025-05-08 19:43:31 -0400

    ajustes de colores y visuales

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AboutComponent } from '../../about/about.component';
 import { CardLenguajesComponent } from '../card-lenguajes/card-lenguajes.component';
 import { CardProyectosComponent } from '../card-proyectos/card-proyectos.component';
@@ -9,6 +9,26 @@ import { CardProyectosComponent } from '../card-proyectos/card-proyectos.compone
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
-export class InicioComponent {
+export class InicioComponent implements OnInit {
 
+  textoCompleto = "Hola! soy Sebastián";
+  textoMostrar = '';
+  velocidad = 100;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.escribirTexto();
+  }
+
+  escribirTexto() {
+    let index = 0;
+    const intervalo = setInterval(() => {
+      this.textoMostrar += this.textoCompleto[index];
+      index++;
+      if (index === this.textoCompleto.length) {
+        clearInterval(intervalo); // Detiene la animación cuando se completa
+      }
+    }, this.velocidad);
+  }
 }
